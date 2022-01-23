7ef38b0346cf7f535b86c396c4f20b2be646c6e9
caoshipeng <caoshipeng.csp@cainiao.com>
2022-01-23 13:59:46 +0800

    新建maven项目

---
+++ onepiece-start/src/main/java/com/onepiece/start/OnepieceStartApplication.java
@@ -0,0 +1,13 @@
+package com.onepiece.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnepieceStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnepieceStartApplication.class, args);
+    }
+
+}
---
+++ onepiece-start/src/test/java/com/onepiece/start/OnepieceStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onepiece.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnepieceStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
